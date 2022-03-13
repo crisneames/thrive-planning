50731ad127a793c6ad7eca3e7c817341033d05b5
Robert Smith <rob.smithw@hotmail.com>
2022-03-13 09:11:43 -0500

    one last place to change text to white on landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -8,7 +8,7 @@ const LandingPage = () => {
         <div style={{height: '68px'}} className='font-face-gm text-center'>
             <div>
                 <h1 style={{fontSize: '3.2rem'}} className='pt-4'>Have you <b style={{color: 'white'}}>THRĪV'D</b> today?</h1>
-                <p style={{fontSize: '1.3rem'}}>A personalized and stategic plan to identify your emotional triggers,<br /> coping skills, and social support system.</p>
+                <p style={{fontSize: '1.3rem', color: 'white'}}>A personalized and stategic plan to identify your emotional triggers,<br /> coping skills, and social support system.</p>
             </div>
             <Button href="/login" style={{padding: '1rem 1rem', fontSize: '1rem'}}>Get Started</Button>
         </div>
